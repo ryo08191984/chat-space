bd8821ad66baf6c32a87d27947e843471560a39f
ryo mutsuzaki <ryo.mutsuzaki@gmail.com>
2019-06-28 11:40:12 +0900

    グループ作成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
   def index
   end
-
+  
   def new
     @group = Group.new
     @group.users << current_user
@@ -15,9 +15,17 @@ class GroupsController < ApplicationController
       render :new
     end
   end
+  
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
+  end
 
   private
   def group_params
-    params.require(:group).permit(:name, user_ids: [] )
+    params.require(:group).permit(:name, { :user_ids => [] })
   end
 end
